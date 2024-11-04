a509c193a9117aa8b8733dd6b39c7fc992562ba1
Amey Gaikar <gaikaramey04@gmail.com>
2024-11-04 11:44:32 +0530

    Adding bookpages

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 const fileName = fileURLToPath(import.meta.url);
 const __dirName = path.dirname(fileName);
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirName, 'public')));
 app.use('/css', express.static(path.join(__dirName, 'css')));
@@ -25,5 +28,31 @@ app.get("/homepage", (req,res) => {
     res.render("homepage.ejs")
 })
 
+app.post("/book/:id",async (req, res) => {
+  const BookId = req.params.id;
+  const bookData = `https://openlibrary.org/search.json?q=${BookId}`;
+
+  let bookAuthor;
+  let bookTitle;
+  const bookCover = `https://covers.openlibrary.org/b/isbn/${BookId}-M.jpg`;
+try {
+    const result = await axios.get(bookData);
+    bookTitle =  result.data.docs[0].title;
+    bookAuthor = result.data.docs[0].author_name[0];
+
+    res.render("bookPage.ejs", {
+        bookTitle: bookTitle,
+        BookId: BookId,
+        bookAuthor: bookAuthor,
+        bookCover: bookCover
+    })
+
+} catch (error) {
+    console.error(error);
+}
+})
 
 
+app.post("/bookAdded", async (req, res) => {
+    console.log(req);
+})
